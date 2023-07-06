d71daa37986cdc05803d7b34da6f5dcd968ceb79
Biswajit_2600 <biswajitpanda754@gmail.com>
2023-07-07 00:59:05 +0530

    added carousel in skills section

--- JS/main.js
+++ JS/main.js
@@ -14,7 +14,7 @@ var typed = new Typed(".text", {
 // circle skill ////////////////////////
 
 const circles = document.querySelectorAll(".circle");
-circles.forEach(elem => {
+circles.forEach((elem) => {
   var dots = elem.getAttribute("data-dots");
   var marked = elem.getAttribute("data-percent");
   var percent = Math.floor((dots * marked) / 100);
@@ -117,3 +117,124 @@ slideLeftAnchor3.forEach((el) => observer.observe(el));
 
 const slideLeftAnchor4 = document.querySelectorAll(".slide-top-4");
 slideLeftAnchor4.forEach((el) => observer.observe(el));
+
+// skills section carousel ///////////////
+
+const skill_details = document.querySelector(".skill_details");
+const carousel = document.querySelector(".carousel");
+const firstCardWidth = carousel.querySelector(".card").offsetWidth;
+const arrowBtns = document.querySelectorAll(".skill_details i");
+const carouselChildrens = [...carousel.children];
+
+let isDragging = false,
+  isAutoPlay = true,
+  startX,
+  startScrollLeft,
+  timeoutId;
+
+// Get the number of cards that can fit in the carousel at once
+let cardPerView = Math.round(carousel.offsetWidth / firstCardWidth);
+
+// Insert copies of the last few cards to beginning of carousel for infinite scrolling
+carouselChildrens
+  .slice(-cardPerView)
+  .reverse()
+  .forEach((card) => {
+    carousel.insertAdjacentHTML("afterbegin", card.outerHTML);
+  });
+
+// Insert copies of the first few cards to end of carousel for infinite scrolling
+carouselChildrens.slice(0, cardPerView).forEach((card) => {
+  carousel.insertAdjacentHTML("beforeend", card.outerHTML);
+});
+
+// Scroll the carousel at appropriate postition to hide first few duplicate cards on Firefox
+carousel.classList.add("no-transition");
+carousel.scrollLeft = carousel.offsetWidth;
+carousel.classList.remove("no-transition");
+
+// Add event listeners for the arrow buttons to scroll the carousel left and right
+arrowBtns.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    carousel.scrollLeft += btn.id == "left" ? -firstCardWidth : firstCardWidth;
+  });
+});
+
+const dragStart = (e) => {
+  isDragging = true;
+  carousel.classList.add("dragging");
+  // Records the initial cursor and scroll position of the carousel
+  startX = e.pageX;
+  startScrollLeft = carousel.scrollLeft;
+};
+
+const dragging = (e) => {
+  if (!isDragging) return; // if isDragging is false return from here
+  // Updates the scroll position of the carousel based on the cursor movement
+  carousel.scrollLeft = startScrollLeft - (e.pageX - startX);
+};
+
+const dragStop = () => {
+  isDragging = false;
+  carousel.classList.remove("dragging");
+};
+
+const infiniteScroll = () => {
+  // If the carousel is at the beginning, scroll to the end
+  if (carousel.scrollLeft === 0) {
+    carousel.classList.add("no-transition");
+    carousel.scrollLeft = carousel.scrollWidth - 2 * carousel.offsetWidth;
+    carousel.classList.remove("no-transition");
+  }
+  // If the carousel is at the end, scroll to the beginning
+  else if (
+    Math.ceil(carousel.scrollLeft) ===
+    carousel.scrollWidth - carousel.offsetWidth
+  ) {
+    carousel.classList.add("no-transition");
+    carousel.scrollLeft = carousel.offsetWidth;
+    carousel.classList.remove("no-transition");
+  }
+
+  // Clear existing timeout & start autoplay if mouse is not hovering over carousel
+  clearTimeout(timeoutId);
+  if (!skill_details.matches(":hover")) autoPlay();
+};
+
+const autoPlay = () => {
+  // Autoplay the carousel after every 2000 ms
+  timeoutId = setTimeout(() => (carousel.scrollLeft += firstCardWidth), 2000);
+};
+autoPlay();
+
+carousel.addEventListener("mousedown", dragStart);
+carousel.addEventListener("mousemove", dragging);
+document.addEventListener("mouseup", dragStop);
+carousel.addEventListener("scroll", infiniteScroll);
+skill_details.addEventListener("mouseenter", () => clearTimeout(timeoutId));
+skill_details.addEventListener("mouseleave", autoPlay);
+
+// menu for toggling between technical and professional skills
+
+var divs = ["technical_menu", "professional_menu"];
+var visibleDivId = null;
+function toggleVisibility(divId) {
+  if (visibleDivId === divId) {
+    //visibleDivId = null;
+  } else {
+    visibleDivId = divId;
+  }
+  hideNonVisibleDivs();
+}
+function hideNonVisibleDivs() {
+  var i, divId, div;
+  for (i = 0; i < divs.length; i++) {
+    divId = divs[i];
+    div = document.getElementById(divId);
+    if (visibleDivId === divId) {
+      div.style.display = "flex";
+    } else {
+      div.style.display = "none";
+    }
+  }
+}
