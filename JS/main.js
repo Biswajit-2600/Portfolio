43938ef570ace0463076142a4d21148f9bbba3c6
Biswajit <biswajitpanda754@gmail.com>
2023-08-07 18:06:44 +0530

    made changes to the intersection observer

--- JS/main.js
+++ JS/main.js
@@ -99,9 +99,7 @@ const observer = new IntersectionObserver((entries) => {
   entries.forEach((entry) => {
     if (entry.isIntersecting) {
       entry.target.classList.add("show-items");
-    } else {
-      entry.target.classList.remove("show-items");
-    }
+    } 
   });
 });
 
